c97dd83793fc3ef162254d7cfa86ee65e34b65fe
Julien Buret <julien.buret@gmail.com>
2011-07-11 22:57:36 +0200

    end of rename package

--- src/main/java/fr/xebia/profiling/configuration/ClassPattern.java
+++ src/main/java/fr/xebia/profiling/configuration/ClassPattern.java
@@ -10,6 +10,6 @@ public class ClassPattern {
     }
 
     public boolean isClassNameMatch(String classname){
-        return classname.startsWith("fr/jbu/profiling/testclasses/SimpleClasses");
+        return classname.startsWith("fr/xebia/profiling/testclasses/SimpleClasses");
     }
 }
